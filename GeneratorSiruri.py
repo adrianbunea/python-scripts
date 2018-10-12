d4f5582d6c7e1b848f8bcaa2d3ce2b244e3d1951
adrianbunea <adrian_bunea28@yahoo.com>
2018-10-12 18:13:12 +0300

    Created custom function to be used in filter().

--- GeneratorSiruri.py
+++ GeneratorSiruri.py
@@ -1,15 +1,23 @@
 import string
 import random
 
-Neterminale = {'E','T','F'}
-Terminale = {'a','+','*','(',')'}
-Start = 'S'
-P = [('E','E+T'),('E','T'),('T','T*F'),('T','F'),('F','a'),('F','(E)')]
+def filtreaza_reguli_productie(lista, caracter):
+    for regula in lista:
+        if regula[0] == caracter:
+            return True
+        else:
+            return False
 
-# sir = Start
-# while len(sir) < 60:
-#     for caracter in sir:
-#         if caracter in Neterminale:
+neterminale = {'E','T','F'}
+terminale = {'a','+','*','(',')'}
+start = 'S'
+reguli_productie = [('E','E+T'),('E','T'),('T','T*F'),('T','F'),('F','a'),('F','(E)')]
+
+sir = start
+while len(sir) < 60:
+    for caracter in sir:
+        if caracter in neterminale:
+            lista_filtrata = filter(filtreaza_reguli_productie(reguli_productie, caracter), reguli_productie)
             
-Caracter = P[0][0]
+Caracter = reguli_productie[0][0]
 print(Caracter)
