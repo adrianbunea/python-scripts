6a225cdd973aaeba99cb84bd59a2a0891b75055c
adrianbunea <adrian_bunea28@yahoo.com>
2018-10-12 22:29:37 +0300

    Generator kind of does something. Sometimes...

--- GeneratorSiruri.py
+++ GeneratorSiruri.py
@@ -1,30 +1,26 @@
 import string
 from random import randrange
 
-def filtreaza_regula_productie(caracter, regula_productie):
-        if caracter in regula_productie[0]:
-            return True
-        else:
-            return False
-
 neterminale = ['E','T','F']
 terminale = ['a','+','*','(',')']
 start = 'E'
 reguli_productie = [('E','E+T'),('E','T'),('T','T*F'),('T','F'),('F','a'),('F','(E)')]
 
 sir = start
-# while len(sir) < 60:
-#     for caracter in sir:
-#         if caracter in neterminale:
-#             lista_filtrata = filter(filtreaza_reguli_productie(caracter), reguli_productie)
-while len(sir) < 60:           
-    for i in range(0, len(sir)):
+lungime_sir = len(sir)
+
+while lungime_sir < 60:  
+    if all([caracter not in sir for caracter in neterminale]):
+        break
+    lungime_sir = len(sir)     
+    for i in range(0, lungime_sir):
         caracter = sir[i]
         if caracter in neterminale:
-            lista_filtrata = ""
+            lista_filtrata = []
             for regula in reguli_productie:
                 if caracter in regula[0]:
-                    lista_filtrata = lista_filtrata + caracter
+                    substitutie = regula[1]
+                    lista_filtrata.append(substitutie)
                     index = randrange(0,len(lista_filtrata))
                     sir = sir.replace(sir[i],lista_filtrata[index])
 
