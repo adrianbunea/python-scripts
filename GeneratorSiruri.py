31996be211260b95ac824967d7bc0400268b91cf
adrianbunea <adrian_bunea28@yahoo.com>
2018-10-12 17:59:54 +0300

    Tried some stuff.

--- GeneratorSiruri.py
+++ GeneratorSiruri.py
@@ -1,5 +1,15 @@
 import string
+import random
 
 Neterminale = {'E','T','F'}
 Terminale = {'a','+','*','(',')'}
 Start = 'S'
+P = [('E','E+T'),('E','T'),('T','T*F'),('T','F'),('F','a'),('F','(E)')]
+
+# sir = Start
+# while len(sir) < 60:
+#     for caracter in sir:
+#         if caracter in Neterminale:
+            
+Caracter = P[0][0]
+print(Caracter)
