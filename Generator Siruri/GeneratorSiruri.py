66fb8cf016eab4dcda55be17fb04906878c939b0
adrianbunea <adrian_bunea28@yahoo.com>
2018-10-17 21:36:14 +0300

    Changed substitution algorithm. Changed text file parsing function, also changed the way it is used so it can be more easily replaced with a different one. Changed grammar files and added 2 new ones to fit with the new parsing algorithm. All is working fine.

--- Generator Siruri/GeneratorSiruri.py
+++ Generator Siruri/GeneratorSiruri.py
@@ -1,68 +1,71 @@
 import string
 from random import randrange
 
-def sterge_lambda():
-    global sir
-    sir = sir.replace('_', '')
+stanga, dreapta = 0, 1
 
-def genereaza_lista_substitutii(caracter):
+def sterge_lambda(sir):
+    sir = sir.replace('_', '')
+    return sir
+
+def genereaza_lista_substitutii(caractere):
     lista_optiuni = []
     for regula in reguli_productie:
-        if caracter in regula[0]:
-            regula_posibila = regula[1]
-            lista_optiuni.append(regula_posibila)
+        if caractere in regula[stanga]:
+            substitutie_posibila = regula[dreapta]
+            lista_optiuni.append(substitutie_posibila)
     return lista_optiuni
 
-def substituie(caracter):
-    lista_optiuni = genereaza_lista_substitutii(caracter)
+def substituie(caractere):
+    lista_optiuni = genereaza_lista_substitutii(caractere)
 
     global sir
-    sterge_lambda()
+    sir = sterge_lambda(sir)
     while lista_optiuni != []:
         index = randrange(0,len(lista_optiuni))
         if len(sir) + len(lista_optiuni[index])-1 > 60:
             lista_optiuni.remove(lista_optiuni[index])
         else: 
-            sir = sir.replace(caracter,lista_optiuni[index],1)
+            sir = sir.replace(caractere,lista_optiuni[index],1)
+            sir = sterge_lambda(sir)
             return True
     return False
 
 def citeste_fisier(nume_fisier):
-    gramatica = open(nume_fisier, 'r')
-    gramatica = gramatica.read().replace("\n", '')
+    gramatica = open(nume_fisier, 'r').read().replace('\n', '')
 
-    global neterminale
-    neterminale = gramatica[gramatica.find("neterminale = ")+14 : gramatica.find("terminale", gramatica.find("neterminale = ")+14)]
-    neterminale = neterminale[neterminale.find('[\'')+2: neterminale.find('\']')]
-    neterminale = neterminale.split('\',\'')
+    neterminale = gramatica[gramatica.find("NETERMINALE:"): gramatica.find("TERMINALE:", gramatica.find("NETERMINALE:")+len("NETERMINALE:"))]
+    gramatica = gramatica.replace(gramatica[gramatica.find("NETERMINALE:"): gramatica.find("TERMINALE:", gramatica.find("NETERMINALE:")+len("NETERMINALE:"))], '')
+    neterminale = neterminale.replace("NETERMINALE:", '')
+    neterminale = neterminale.split(' ')
 
-    global terminale
-    terminale = gramatica[gramatica.find("terminale = ", gramatica.find("neterminale = ")+14)+12 : gramatica.find("start")]
-    terminale = terminale[terminale.find('[\'')+2: terminale.find('\']')]
-    terminale = terminale.split('\',\'')
+    terminale = gramatica[gramatica.find("TERMINALE:") : gramatica.find("START:", gramatica.find("TERMINALE:")+len("TERMINALE:"))]
+    gramatica = gramatica.replace(gramatica[gramatica.find("TERMINALE:") : gramatica.find("START:", gramatica.find("TERMINALE:")+len("TERMINALE:"))], '')
+    terminale = terminale.replace("TERMINALE:", '')
+    terminale = terminale.split(' ')
 
-    global start
-    start = gramatica[gramatica.find("start = ")+8 : gramatica.find("reguli_productie")]
-    start = start.strip('\'')
+    start = gramatica[gramatica.find("START:") : gramatica.find("REGULI_PRODUCTIE:", gramatica.find("START:")+len("START:"))]
+    start = start.replace("START:", '')
 
-    global reguli_productie
-    reguli_productie = gramatica[gramatica.find("reguli_productie = ")+19 :]
-    reguli_productie = reguli_productie[reguli_productie.find('[(')+1: reguli_productie.find(')]')]
-    reguli_productie_temp = reguli_productie.split('),(')
     reguli_productie = []
-    for regula_temp in reguli_productie_temp:
-        regula = []
-        regula_temp = regula_temp.strip('\'')
-        regula.append(regula_temp.split('\' \'')[0])
-        regula.append(regula_temp.split('\' \'')[1])
-        regula = tuple(regula)
-        reguli_productie.append(regula)
+    reguli_productie_temp = gramatica[gramatica.find("REGULI_PRODUCTIE:"):]
+    reguli_productie_temp = reguli_productie_temp.replace("REGULI_PRODUCTIE:", '')
+    reguli_productie_temp = reguli_productie_temp.split(';')
+    for regula in reguli_productie_temp:
+        termen_stanga = regula.split('->')[stanga].replace(' ','')
+        termeni_dreapta = regula.split('->')[dreapta].replace(' ', '').split('|')
+        for termen in termeni_dreapta:
+            reguli_productie.append((termen_stanga, termen))
 
-neterminale = ""
-terminale = ""
-start = ''
-reguli_productie = ""
-citeste_fisier("Gramatica2.txt")
+    # gramatica = tuple(neterminale,terminale,start,reguli_productie)
+    gramatica = {"neterminale" : neterminale, "terminale" : terminale, "start" : start, "reguli_productie" : reguli_productie}
+    return gramatica
+
+# START
+gramatica = citeste_fisier("Gramatica2.txt")
+neterminale = gramatica["neterminale"]
+terminale = gramatica["terminale"]
+start = gramatica["start"]
+reguli_productie = gramatica["reguli_productie"]
 
 sir = start
 lungime_sir = len(sir)
@@ -70,12 +73,20 @@ lungime_sir = len(sir)
 substitutie = True
 
 while substitutie == True:
+    index = len(sir)
     substitutie = False
-    for caracter in sir:
-        if caracter in neterminale:
-           substitutie = substituie(caracter)
+    # for caracter in sir:
+    #     if caracter in neterminale:
+    #        substitutie = substituie(caracter)
+    for regula in reguli_productie:
+        if sir.find(regula[stanga]) >= 0 and sir.find(regula[stanga]) < index:
+            index = sir.find(regula[stanga])
+            caractere = regula[stanga]
+    if index < len(sir):
+        substitutie = substituie(caractere)
 
-sterge_lambda()
+
+sir = sterge_lambda(sir)
 
 print("Sir: " + sir)
 print("Sirul are " + str(len(sir)) + " caractere.")
