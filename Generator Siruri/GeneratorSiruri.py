cb190e971c7f0f88a9e0da724bc65b122a213f0f
adrianbunea <adrian_bunea28@yahoo.com>
2018-12-09 18:55:04 +0200

    Nothing important

--- Generator Siruri/GeneratorSiruri.py
+++ Generator Siruri/GeneratorSiruri.py
@@ -1,37 +1,39 @@
 from Functii import stanga, dreapta, citeste_fisier, sterge_lambda
 
-import string
 import argparse
 from random import randrange
 
 parser = argparse.ArgumentParser()
-parser.add_argument("nume_fisier", help = "alege un fisier text din care sa fie citita gramatica utilizata")
+parser.add_argument("nume_fisier", help="alege un fisier text din care sa fie citita gramatica utilizata")
 args = parser.parse_args()
 
 sir = ""
 
+
 def genereaza_lista_substitutii(caractere):
-    lista_optiuni = []
-    for regula in reguli_productie:
-        if caractere in regula[stanga]:
-            substitutie_posibila = regula[dreapta]
-            lista_optiuni.append(substitutie_posibila)
-    return lista_optiuni
+	lista_optiuni = []
+	for regula in reguli_productie:
+		if caractere in regula[stanga]:
+			substitutie_posibila = regula[dreapta]
+			lista_optiuni.append(substitutie_posibila)
+	return lista_optiuni
+
 
 def substituie(caractere):
-    lista_optiuni = genereaza_lista_substitutii(caractere)
+	lista_optiuni = genereaza_lista_substitutii(caractere)
 
-    global sir
-    sir = sterge_lambda(sir)
-    while lista_optiuni != []:
-        index = randrange(0,len(lista_optiuni))
-        if len(sir) + len(lista_optiuni[index])-1 > 60:
-            lista_optiuni.remove(lista_optiuni[index])
-        else: 
-            sir = sir.replace(caractere,lista_optiuni[index],1)
-            sir = sterge_lambda(sir)
-            return True
-    return False
+	global sir
+	sir = sterge_lambda(sir)
+	while lista_optiuni != []:
+		index = randrange(0,len(lista_optiuni))
+		if len(sir) + len(lista_optiuni[index])-1 > 60:
+			lista_optiuni.remove(lista_optiuni[index])
+		else:
+			sir = sir.replace(caractere,lista_optiuni[index],1)
+			sir = sterge_lambda(sir)
+			return True
+	return False
+
 
 # START
 gramatica = citeste_fisier(args.nume_fisier)
@@ -42,18 +44,18 @@ reguli_productie = gramatica["reguli_productie"]
 
 sir = start
 lungime_sir = len(sir)
-   
+
 substitutie = True
 
 while substitutie == True:
-    index = len(sir)
-    substitutie = False
-    for regula in reguli_productie:
-        if sir.find(regula[stanga]) >= 0 and sir.find(regula[stanga]) < index:
-            index = sir.find(regula[stanga])
-            caractere = regula[stanga]
-    if index < len(sir):
-        substitutie = substituie(caractere)
+	index = len(sir)
+	substitutie = False
+	for regula in reguli_productie:
+		if sir.find(regula[stanga]) >= 0 and sir.find(regula[stanga]) < index:
+			index = sir.find(regula[stanga])
+			caractere = regula[stanga]
+	if index < len(sir):
+		substitutie = substituie(caractere)
 
 sir = sterge_lambda(sir)
 
